f4918befcd1d7e6cef5ce380132f282c0329a830
bitetheapple <dylananddaymee@icloud.com>
2026-04-22 16:59:28 -0700

    Add files via upload

---
+++ systemreservedunlocker.bat
@@ -0,0 +1,8 @@
+diskpart /s diskpartcmds.txt
+Z:
+takeown /F Z:\Boot /R /D Y
+icacls Z:\Boot /grant Administrators:F /T
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hidden /t REG_DWORD /d 1 /f
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowSuperHidden /t REG_DWORD /d 1 /f
+taskkill /f /im explorer.exe
+start explorer.exe
