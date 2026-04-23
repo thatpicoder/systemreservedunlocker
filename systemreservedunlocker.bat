ea1c55e063038c0139402fdf6d205ccc691078c2
bitetheapple <dylananddaymee@icloud.com>
2026-04-23 15:55:11 -0500

    Update systemreservedunlocker.bat

--- systemreservedunlocker.bat
+++ systemreservedunlocker.bat
@@ -4,5 +4,3 @@ takeown /F Z:\Boot /R /D Y
 icacls Z:\Boot /grant Administrators:F /T
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hidden /t REG_DWORD /d 1 /f
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowSuperHidden /t REG_DWORD /d 1 /f
-taskkill /f /im explorer.exe
-start explorer.exe
